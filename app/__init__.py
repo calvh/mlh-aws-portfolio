c139ca9796d6414f2e8fe46f1ed6b7ca79f7dda8
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-06-21 21:27:44 -0400

    Add code for sqlite3 database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
+import os
+
 from flask import Flask, Response
 from flask import render_template
 
+from app import db
 from app.portfolio import portfolio
 
 app = Flask(__name__)
-
+app.config["DATABASE"] = os.path.join(os.getcwd(), "flask.sqlite")
+db.init_app(app)
 app.register_blueprint(portfolio)
+
 
 @app.route("/health")
 def health():
