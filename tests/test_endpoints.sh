9318ea1a62aa2e66a1de9eee280228213e9afdc7
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-06-22 18:45:12 -0400

    Add tests for endpoints

--- tests/test_endpoints.sh
+++ tests/test_endpoints.sh
@@ -1,55 +1,69 @@
 #!/bin/bash
 
+# run "flask init-db" before doing tests
+
 root_url="http://localhost:5000/"
-auth_url="${root_url}""auth/"
-echo "${auth_url}"
+auth_url="${root_url}auth/"
 username1=john
 username2=jane
 password1=john
 password2=jane
 
 function get_endpoint() {
-    local command="curl -I -s -o /dev/null -w %{http_code} "${2}""
-    echo "${1}" "$(${command})"
+    args=(-I -s -o /dev/null -w %{http_code} "${2}")
+    echo "${1}" $(curl "${args[@]}")
 }
 
 function auth_endpoint() {
-    local command="curl -s -L -X POST '"${2}"'"
-    command+="-F 'username=\""${3}"\"'" 
-    command+="-F 'password=\""${4}"\"'"
-    echo "${command}"
-    echo "${1}""$(${command})"
+    args=(-s -o /dev/null -w %{http_code} -X POST "${2}" -F username=${3} -F password=${4})
+    echo "${1}" $(curl "${args[@]}")
 }
 
 # test index page
-get_endpoint "Testing '/' ..." "${root_url}"
+get_endpoint "GET '/' ..." "${root_url}"
 
 # test projects page
-get_endpoint "Testing '/projects/' ..." "${root_url}""projects/"
+get_endpoint "GET '/projects/' ..." "${root_url}projects/"
 
 # test contact page
-get_endpoint "Testing '/contact/' ..." "${root_url}""contact/"
+get_endpoint "GET '/contact/' ..." "${root_url}contact/"
 
 # test resume page
-get_endpoint "Testing '/resume/' ..." "${root_url}""resume/"
+get_endpoint "GET '/resume/' ..." "${root_url}resume/"
 
 # test 404 page
-get_endpoint "Testing '/oops/' ..." "${root_url}""oops/"
+get_endpoint "GET '/oops/' ..." "${root_url}oops/"
 
 # test register no username no password
+auth_endpoint "POST '/auth/register' username='' password='' ..." "${auth_url}register" "" ""
+
 # test register no username with password
+auth_endpoint "POST '/auth/register' username='' password='john' ..." "${auth_url}register" "" "john"
+
 # test register username with no password
+auth_endpoint "POST '/auth/register' username='john' password='' ..." "${auth_url}register" "john" ""
+
 # test register new user
+auth_endpoint "POST '/auth/register' username='john' password='john' ..." "${auth_url}register" "john" "john"
 
 # test register existing user
-# auth_endpoint "Testing register with empty credentials..." "john" "john"
+auth_endpoint "POST '/auth/register' username='john' password='john' ..." "${auth_url}register" "john" "john"
+
 
 # test login no username no password
+auth_endpoint "POST '/auth/login' username='' password='' ..." "${auth_url}login" "" ""
+
 # test login no username with password
+auth_endpoint "POST '/auth/login' username='' password='john' ..." "${auth_url}login" "" "john"
+
 # test login username with no password
+auth_endpoint "POST '/auth/login' username='john' password='' ..." "${auth_url}login" "john" ""
+
 # test login user
+auth_endpoint "POST '/auth/login' username='john' password='john' ..." "${auth_url}login" "john" "john"
+
 # test login non-existing user
+auth_endpoint "POST '/auth/login' username='jane' password='jane' ..." "${auth_url}login" "jane" "jane"
+
 # test login wrong password
-
-
-# test http redirects to https
+auth_endpoint "POST '/auth/login' username='john' password='jane' ..." "${auth_url}login" "john" "jane"
