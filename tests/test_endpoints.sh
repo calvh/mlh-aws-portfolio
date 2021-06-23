7ecd26c50cb2ac7f3cf098c7c129013f8f39f71f
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-06-22 23:26:21 -0400

    Add success failure messages to tests

--- tests/test_endpoints.sh
+++ tests/test_endpoints.sh
@@ -13,58 +13,60 @@ password2=passwd2
 
 function get_endpoint() {
     args=(-I -s -o /dev/null -w %{http_code} "${2}")
-    echo "${1}" $(curl "${args[@]}")
+    status=$(curl "${args[@]}")
+    echo "${1} ${status}" "$(if [[ "${status}" == "${3}" ]]; then echo "Success"; else echo "Error"; fi)"
 }
 
 function auth_endpoint() {
     args=(-s -o /dev/null -w %{http_code} -X POST "${2}" -F username=${3} -F password=${4})
-    echo "${1}" $(curl "${args[@]}")
+    status=$(curl "${args[@]}")
+    echo "${1} ${status}" "$(if [[ "${status}" == "${5}" ]]; then echo "Success"; else echo "Error"; fi)"
 }
 
 # test index page
-get_endpoint "GET '/' ..." "${root_url}"
+get_endpoint "GET '/' ..." "${root_url}" 200
 
 # test projects page
-get_endpoint "GET '/projects/' ..." "${root_url}projects/"
+get_endpoint "GET '/projects/' ..." "${root_url}projects/" 200
 
 # test contact page
-get_endpoint "GET '/contact/' ..." "${root_url}contact/"
+get_endpoint "GET '/contact/' ..." "${root_url}contact/" 200
 
 # test resume page
-get_endpoint "GET '/resume/' ..." "${root_url}resume/"
+get_endpoint "GET '/resume/' ..." "${root_url}resume/" 200
 
 # test 404 page
-get_endpoint "GET '/oops/' ..." "${root_url}oops/"
+get_endpoint "GET '/oops/' ..." "${root_url}oops/" 404
 
 # test register no username no password
-auth_endpoint "POST /auth/register/' username='' password='' ..." "${register_url}" "" ""
+auth_endpoint "POST /auth/register/' username='' password='' ..." "${register_url}" "" "" 418
 
 # test register no username with password
-auth_endpoint "POST '/auth/register/' username='' password='${password1}' ..." "${register_url}" "" "${password1}"
+auth_endpoint "POST '/auth/register/' username='' password='${password1}' ..." "${register_url}" "" "${password1}" 418
 
 # test register username with no password
-auth_endpoint "POST '/auth/register/' username='${username1}' password='' ..." "${register_url}" "${username1}" ""
+auth_endpoint "POST '/auth/register/' username='${username1}' password='' ..." "${register_url}" "${username1}" "" 418
 
 # test register new user
-auth_endpoint "POST '/auth/register/' username='${username1}' password='${password1}' ..." "${register_url}" "${username1}" "${password1}"
+auth_endpoint "POST '/auth/register/' username='${username1}' password='${password1}' ..." "${register_url}" "${username1}" "${password1}" 200
 
 # test register existing user
-auth_endpoint "POST '/auth/register/' username='${username1}' password='${password1}' ..." "${register_url}" "${username1}" "${password1}"
+auth_endpoint "POST '/auth/register/' username='${username1}' password='${password1}' ..." "${register_url}" "${username1}" "${password1}" 418
 
 # test login no username no password
-auth_endpoint "POST '/auth/login/' username='' password='' ..." "${login_url}" "" ""
+auth_endpoint "POST '/auth/login/' username='' password='' ..." "${login_url}" "" "" 418
 
 # test login no username with password
-auth_endpoint "POST '/auth/login/' username='' password='${password1}' ..." "${login_url}" "" "${password1}"
+auth_endpoint "POST '/auth/login/' username='' password='${password1}' ..." "${login_url}" "" "${password1}" 418
 
 # test login username with no password
-auth_endpoint "POST '/auth/login/' username='${username1}' password='' ..." "${login_url}" "${username1}" ""
+auth_endpoint "POST '/auth/login/' username='${username1}' password='' ..." "${login_url}" "${username1}" "" 418
 
 # test login user
-auth_endpoint "POST '/auth/login/' username='${username1}' password='${password1}' ..." "${login_url}" "${username1}" "${password1}"
+auth_endpoint "POST '/auth/login/' username='${username1}' password='${password1}' ..." "${login_url}" "${username1}" "${password1}" 200
 
 # test login non-existing user
-auth_endpoint "POST '/auth/login/' username='${username2}' password='${password2}' ..." "${login_url}" "${username2}" "${password2}"
+auth_endpoint "POST '/auth/login/' username='${username2}' password='${password2}' ..." "${login_url}" "${username2}" "${password2}" 418
 
 # test login wrong password
-auth_endpoint "POST '/auth/login/' username='${username1}' password='${password2}' ..." "${login_url}" "${username1}" "${password2}"
+auth_endpoint "POST '/auth/login/' username='${username1}' password='${password2}' ..." "${login_url}" "${username1}" "${password2}" 418
