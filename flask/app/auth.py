39e4f1286076f49551ae49877e4e1268eaca62f2
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-07-05 13:50:43 -0400

    Convert to Docker format
    Change Flask pattern to app factory
    Change database methods in 'auth' to use ORM

--- flask/app/auth.py
+++ flask/app/auth.py
@@ -3,7 +3,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app.db import get_db
+from app.models import db, User
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -12,24 +12,18 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif User.query.filter(User.username == username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            db.session.add(User(username=username, password=generate_password_hash(password)))
+            db.session.commit()
             flash("Register successful, please login.")
             return redirect(url_for('auth.login'))
 
@@ -43,21 +37,19 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+
+        user = User.query.filter(User.username == username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif check_password_hash(user['password'], password) is False:
+        elif check_password_hash(user.password, password) is False:
             error = 'Incorrect password.'
 
         if error is None:
             session.clear()
-            session['username'] = user['username']
-            session["id"] = user["id"]
+            session['username'] = user.username
+            session["id"] = user.id
             return redirect(url_for('portfolio.index'))
 
         flash(error)
