780b8202d9031efeccbcaaf61f1b4b77ba4ab028
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-07-07 23:09:04 -0400

    Fix prod test

--- prod_test.sh
+++ prod_test.sh
@@ -6,7 +6,11 @@ root_url="https://calvh.duckdns.org/"
 function get_endpoint() {
     args=(-I -s -o /dev/null -w %{http_code} "${2}")
     status=$(curl "${args[@]}")
-    echo "${1} ${status}" "$(if [[ "${status}" == "${3}" ]]; then exit 0; else exit 1; fi)"
+    if [[ "${status}" == "${3}" ]]; then
+        exit 0 
+    else
+        exit 1
+    fi
 }
 
 # test index page
