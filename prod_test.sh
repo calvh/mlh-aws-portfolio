ed0ab6c54191bcb3dfda634c70b3e5dbf3544daa
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-07-07 23:27:21 -0400

    Fix prod_test

--- prod_test.sh
+++ prod_test.sh
@@ -7,8 +7,10 @@ function get_endpoint() {
     args=(-I -s -m 30 -o /dev/null -w %{http_code} "${2}")
     status=$(curl "${args[@]}")
     if [[ "${status}" == "${3}" ]]; then
+        echo "All systems go."
         exit 0 
     else
+        echo "Something went wrong."
         exit 1
     fi
 }
