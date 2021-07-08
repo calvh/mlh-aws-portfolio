1fc84be63d7b60579cc6f8573842332f1c5a8385
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-07-07 23:50:28 -0400

    Add additional production tests

--- prod_test.sh
+++ prod_test.sh
@@ -4,16 +4,28 @@
 root_url="https://calvh.duckdns.org/"
 
 function get_endpoint() {
+    
     args=(-I -s -m 30 -o /dev/null -w %{http_code} "${2}")
     status=$(curl "${args[@]}")
     if [[ "${status}" == "${3}" ]]; then
-        echo "All systems go."
-        exit 0 
+        echo "Test ${1} ${status} succeeded."
     else
-        echo "Something went wrong."
+        echo "Test ${1} ${status} failed."
         exit 1
     fi
 }
 
 # test index page
 get_endpoint "GET '/' ..." "${root_url}" 200
+
+# test projects page
+get_endpoint "GET '/projects/' ..." "${root_url}projects/" 200
+
+# test contact page
+get_endpoint "GET '/contact/' ..." "${root_url}contact/" 200
+
+# test resume page
+get_endpoint "GET '/resume/' ..." "${root_url}resume/" 200
+
+# test 404 page
+get_endpoint "GET '/oops/' ..." "${root_url}oops/" 404
